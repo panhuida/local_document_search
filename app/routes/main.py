2c4ed7d260d449c210d529e5269808bc70f573f7
panhuida <panhuida@qq.com>
2025-09-10 22:09:07 +0800

    错误记录展示页面优化

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app.models import ConversionError
 
 bp = Blueprint('main', __name__)
@@ -17,5 +17,25 @@ def search_page():
 
 @bp.route('/errors')
 def errors_page():
-    errors = ConversionError.query.order_by(ConversionError.created_at.desc()).all()
-    return render_template('errors.html', errors=errors)
+    query = ConversionError.query
+
+    # Search by filename
+    file_name_search = request.args.get('file_name', '')
+    if file_name_search:
+        query = query.filter(ConversionError.file_name.ilike(f'%{file_name_search}%'))
+
+    # Filter by date
+    date_from = request.args.get('date_from', '')
+    if date_from:
+        query = query.filter(ConversionError.updated_at >= date_from)
+    
+    date_to = request.args.get('date_to', '')
+    if date_to:
+        query = query.filter(ConversionError.updated_at <= date_to)
+
+    # Sort by updated_at
+    query = query.order_by(ConversionError.updated_at.desc())
+
+    errors = query.all()
+
+    return render_template('errors.html', errors=errors, file_name_search=file_name_search, date_from=date_from, date_to=date_to)
